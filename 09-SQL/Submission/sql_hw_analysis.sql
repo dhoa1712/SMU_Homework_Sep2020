3b664239ac0480b8af21ffe5417e0ab03b4ad618
dhoa1712 <dhoa1712@gmail.com>
2020-12-03 17:10:49 -0600

    hw

--- 09-SQL/Submission/sql_hw_analysis.sql
+++ 09-SQL/Submission/sql_hw_analysis.sql
@@ -83,10 +83,10 @@ where
 --8 In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 select 
 	last_name, 
-	count(*)
+	count(*) as name_count
 from 
 	employees
 group by 
 	last_name
 order by 
-	last_name desc
+	name_count desc
